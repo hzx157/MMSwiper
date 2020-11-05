6805da31812b513fbe1866f2d4a33b9d61a168c8
peiming zhang <zhangpeiming1991@gmail.com>
2020-11-05 10:08:57 +0800

    xib

--- MMSwiperDemo/ViewController.m
+++ MMSwiperDemo/ViewController.m
@@ -11,42 +11,47 @@
 #import <SDWebImage/UIImageView+WebCache.h>
 
 @interface ViewController ()<MMSwiperDelegate>
+@property (weak, nonatomic) IBOutlet MMSwiper *carouselView2;
 @property (nonatomic,strong) MMSwiper *carouselView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
-       [super viewDidLoad];
+    [super viewDidLoad];
     
-       _carouselView = [[MMSwiper alloc]initWithFrame:CGRectMake(0, 80, self.view.bounds.size.width, 200)];
-        _carouselView.delegate = self;
-        [self.view addSubview:_carouselView];
-       _carouselView.models = [self setupDataModels];
-    
-    
-        MMSwiper*m1View = [[MMSwiper alloc]initWithFrame:CGRectMake(0, 300, self.view.bounds.size.width, 200)];
-        m1View.delegate = self;
-        m1View.isAuto = YES; //设置自动播放
-        m1View.isInfinite = YES;  //设置是否无限循环
-        m1View.pageMode = MMSwiperPageModeBottomRight; //设置page的位置
-        m1View.pageBottomSpacing = 20.0f; //设置到底部的距离
-        m1View.timeInterval = 4.0f; //设置播放时间
-        m1View.currentPageIndicatorTintColor = [UIColor blueColor];
-        m1View.pageIndicatorTintColor = [UIColor lightTextColor];
-        [self.view addSubview:m1View];
-        m1View.models = [self setupDataModels]; //自己可以设定，传什么数组都可以
-       
-       MMSwiper*m2View = [[MMSwiper alloc]initWithFrame:CGRectMake(0, 520, self.view.bounds.size.width, 200)];
-       m2View.delegate = self;
-       m2View.isInfinite = YES;  //设置是否无限循环
-       m2View.pageMode = MMSwiperPageModeBottomLeft; //设置page的位置
-       m2View.pageBottomSpacing = 30.0f; //设置到底部的距离
-       m2View.placeHolderImage = [UIImage imageNamed:@"组54"];
-       [self.view addSubview:m2View];
-       m2View.models = [self setupDataModels];
+    _carouselView = [[MMSwiper alloc]initWithFrame:CGRectMake(0, 80, self.view.bounds.size.width, 200)];
+    _carouselView.delegate = self;
+    [self.view addSubview:_carouselView];
+    _carouselView.models = [self setupDataModels];
     
     
+    MMSwiper*m1View = [[MMSwiper alloc]initWithFrame:CGRectMake(0, 300, self.view.bounds.size.width, 200)];
+    m1View.delegate = self;
+    m1View.isAuto = YES; //设置自动播放
+    m1View.isInfinite = YES;  //设置是否无限循环
+    m1View.pageMode = MMSwiperPageModeBottomRight; //设置page的位置
+    m1View.pageBottomSpacing = 20.0f; //设置到底部的距离
+    m1View.timeInterval = 4.0f; //设置播放时间
+    m1View.currentPageIndicatorTintColor = [UIColor blueColor];
+    m1View.pageIndicatorTintColor = [UIColor lightTextColor];
+    [self.view addSubview:m1View];
+    m1View.models = [self setupDataModels]; //自己可以设定，传什么数组都可以
+    
+    MMSwiper*m2View = [[MMSwiper alloc]initWithFrame:CGRectMake(0, 520, self.view.bounds.size.width, 200)];
+    m2View.delegate = self;
+    m2View.isInfinite = YES;  //设置是否无限循环
+    m2View.pageMode = MMSwiperPageModeBottomLeft; //设置page的位置
+    m2View.pageBottomSpacing = 30.0f; //设置到底部的距离
+    m2View.placeHolderImage = [UIImage imageNamed:@"组54"];
+    [self.view addSubview:m2View];
+    m2View.models = [self setupDataModels];
+    
+    _carouselView2.delegate = self;
+    [self.view addSubview:_carouselView2];
+    _carouselView2.isInfinite = YES;  //设置是否无限循环
+    _carouselView2.isAuto = YES; //设置自动播放
+    _carouselView2.models = [self setupDataModels];
 }
 
 
@@ -61,15 +66,15 @@
 }
 
 -(NSMutableArray *)setupDataModels{
-        
+    
     NSMutableArray * dataSource = [NSMutableArray arrayWithCapacity:4];
     [dataSource addObject:[[MMSwiperModel alloc]initWithUrl:@"https://t1.hxzdhn.com/uploads/tu/202003/9999/50ff460659.jpg"]];
     [dataSource addObject:[[MMSwiperModel alloc]initWithUrl:@"https://t1.hxzdhn.com/uploads/tu/202003/9999/f876ab9777.jpg"]];
     [dataSource addObject:[[MMSwiperModel alloc]initWithUrl:@"https://t1.hxzdhn.com/uploads/tu/202003/9999/d056be13c8.jpg"]];
     [dataSource addObject:[[MMSwiperModel alloc]initWithUrl:@"https://t1.hxzdhn.com/uploads/tu/202003/9999/43862647ba.jpg"]];
-        
+    
     return dataSource;
-        
+    
 }
 
 
@@ -95,8 +100,8 @@
     if(self = [super init]){
         self.url = url;
         self.title = title;
-       }
-       return self;
+    }
+    return self;
 }
 
 @end
